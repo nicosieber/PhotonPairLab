a767edfde6e1559fe9ac1e9d062f4d7f21a90ed1
nicosieber <nico.sieber@gmx.net>
2025-05-04 18:49:43 +0200

    deleted some leftover code

--- photonpairlab/__init__.py
+++ photonpairlab/__init__.py
@@ -1,4 +1,4 @@
-from . import crystal
-from . import laser
 from . import materials
+from . import laser
+from . import crystal
 from . import spdc
--- photonpairlab/spdc/__init__.py
+++ photonpairlab/spdc/__init__.py
@@ -1,4 +1,3 @@
-from .spdc_simulation import SPDC_Simulation
 from .simulation import SPDC_Simulation
 from .analysis import SPDC_Analyzer
 from .plotting import SPDC_Plotter
