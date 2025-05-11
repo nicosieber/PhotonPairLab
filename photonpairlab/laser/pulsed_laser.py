ae75af60449cfed8b98a30167b609ecfedf28219
nicosieber <nico.sieber@gmx.net>
2025-05-11 10:56:32 +0200

    minor changes

--- photonpairlab/laser/pulsed_laser.py
+++ photonpairlab/laser/pulsed_laser.py
@@ -4,7 +4,7 @@ class PulsedLaser(LaserBase):
     """
     Represents a pulsed laser.
     """
-    def __init__(self, wavelength, pulse_duration):
+    def __init__(self, wavelength, pulse_duration=None, repetition_rate=None):
         """
         Initializes the PulsedLaser object.
 
@@ -13,6 +13,8 @@ class PulsedLaser(LaserBase):
             pulse_duration (float): Pulse duration in seconds (s).
         """
         super().__init__(wavelength)
-        
+
         self.pulse_duration = pulse_duration  # Pulse duration (s)
         self.bandwidth_wavelength = self.pulse_duration_to_bandwidth_wavelength(self.pulse_duration, self.lambda_2w)  # Bandwidth (m)
+        
+        self.repetition_rate = repetition_rate  # Repetition rate (Hz)
